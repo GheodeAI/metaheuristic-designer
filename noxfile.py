17c792e2f67cf4c613cdb448d1eff1cd6f3129e8
eugenioLR <eugeniolorente0@gmail.com>
2023-09-18 12:20:00 +0200

    removed extra line in nox tests

--- noxfile.py
+++ noxfile.py
@@ -8,4 +8,3 @@ def test(session):
     session.install('.')
     session.install('pytest', 'coverage')
     session.run('coverage', 'run', '--data-file', '.coverage.nox', '--parallel', '-m', 'pytest')
-    session.run('coverage', 'html', '--data-file', '.coverage.nox')
