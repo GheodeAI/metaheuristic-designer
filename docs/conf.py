907f55fc5f48d43ae228889806b6b5c912131623
eugenioLR <eugeniolorente0@gmail.com>
2023-10-16 19:10:10 +0200

    changed version

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2023, Eugenio Lorente-Ramos"
 author = "Eugenio Lorente-Ramos"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1.0"
+release = "0.1.3"
 
 
 # -- General configuration ---------------------------------------------------
