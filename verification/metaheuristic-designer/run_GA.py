c5b200a9aa568419e97424445228648ff90ae382
eugenioLR <eugeniolorente0@gmail.com>
2025-05-26 13:55:11 +0200

    fixed to be similar to pygad

--- verification/metaheuristic-designer/run_GA.py
+++ verification/metaheuristic-designer/run_GA.py
@@ -5,42 +5,44 @@ import pandas as pd
 import metaheuristic_designer as mhd
 
 
-def run_ga(objective, dim, mutation, crossover, parentsel, survsel, nexecs, popsize, nreps, fileout):
+def run_ga(objective, dim, mutation, crossover, parentsel, survsel, ngen, popsize, nreps, fileout):
     match objective:
-        case "SPHERE":
+        case "sphere":
             objfunc = mhd.benchmarks.Sphere(dim, "min")
-        case "RASTRIGIN":
+        case "rastrigin":
             objfunc = mhd.benchmarks.Rastrigin(dim, "min")
-        case "ROSENBROCK":
+        case "rosenbrock":
             objfunc = mhd.benchmarks.Rosenbrock(dim, "min")
-        case "WEIERSTRASS":
+        case "weierstrass":
             objfunc = mhd.benchmarks.Weierstrass(dim, "min")
         case _:
             raise Exception(f'Objective function "{objective}" doesn\'t exist.')
         
     match mutation:
-        case "GAUSSMUT":
+        case "gaussmut":
             mutation_op=mhd.operators.OperatorVector("MutNoise", {"distrib": "Gaussian", "F": 1e-3, "N": 1})
-        case "GAUSSSAMPLE":
-            mutation_op=mhd.operators.OperatorVector("MutNoise", {"distrib": "Gaussian", "F": 1e-3, "N": 1})
+        case "uniformsample":
+            mutation_op=mhd.operators.OperatorVector("MutSample", {"distrib": "Uniform", "min":objfunc.low_lim, "max":objfunc.up_lim, "N": 1})
 
     match crossover:
-        case "ONEPOINT":
+        case "onepoint":
             cross_op=mhd.operators.OperatorVector("1point")
 
-    n_parents = 50
+    n_parents = ngen
     match parentsel:
-        case "BEST":
+        case "best":
             parent_sel=mhd.selectionMethods.ParentSelection("Best", {"amount": n_parents})
+        case "generational":
+            parent_sel=mhd.selectionMethods.ParentSelection("Nothing")
 
-    n_elites = 20        
+    n_elites = 1
     match survsel:
-        case "ELITISM":
+        case "elitism":
             survivor_sel=mhd.selectionMethods.SurvivorSelection("Elitism", {"amount": n_elites})
 
 
     search_strat = mhd.strategies.GA(
-        initializer=mhd.initializers.UniformVectorInitializer(objfunc.vecsize, objfunc.low_lim, objfunc.up_lim, pop_size=popsize),
+        initializer=mhd.initializers.UniformVectorInitializer(objfunc.vecsize, low_lim=objfunc.low_lim, up_lim=objfunc.up_lim, pop_size=popsize),
         mutation_op=mutation_op,
         cross_op=cross_op,
         parent_sel=parent_sel,
@@ -48,17 +50,16 @@ def run_ga(objective, dim, mutation, crossover, parentsel, survsel, nexecs, pops
         params={"pcross": 0.8, "pmut": 0.2},
     )
 
-    alg = mhd.algorithms.GeneralAlgorithm(objfunc, search_strat, params={"stop_cond": "neval", "neval": nexecs, "verbose": True})
+    alg = mhd.algorithms.GeneralAlgorithm(objfunc, search_strat, params={"stop_cond": "ngen", "ngen": ngen, "verbose": False})
 
-    exec_data = pd.DataFrame(columns=["Objective", "Dims", "Mutation", "Crossover", "ParentSel", "SurvSel", "Nexecs", "PopSize", "ExecTime", "fitness"])
+    exec_data = pd.DataFrame(columns=["Objective", "Dims", "Mutation", "Crossover", "ParentSel", "SurvSel", "Ngen", "PopSize", "ExecTime", "fitness"])
+    script_path = pathlib.Path(__file__).parent.resolve()
     for i in range(nreps):
         alg.restart()
         new_population = alg.optimize()
         _, best_fitness = new_population.best_solution()
-        exec_data.loc[i] = [objective, dim, mutation.lower(), crossover.lower(), parentsel.lower(), survsel.lower(), nexecs, popsize, alg.cpu_time_spent, best_fitness]
-    
-    script_path = pathlib.Path(__file__).parent.resolve()
-    exec_data.to_csv(f"{script_path}/../data/{fileout}")
+        exec_data.loc[i] = [objective.lower(), dim, mutation.lower(), crossover.lower(), parentsel.lower(), survsel.lower(), ngen, popsize, alg.cpu_time_spent, best_fitness]
+        exec_data.to_csv(f"{script_path}/../data/{fileout}")
 
 
 
@@ -67,14 +68,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--objective", dest="objective", help="Name of the objective function.", default="Sphere")
     parser.add_argument("-d", "--dim", dest="dim", help="Dimension of the vectors to optimize.", default=3, type=int)
-    parser.add_argument("-m", "--mutation", dest="mut", help="Mutation method to use.", default="GaussMut")
+    parser.add_argument("-m", "--mutation", dest="mut", help="Mutation method to use.", default="UniformSample")
     parser.add_argument("-c", "--crossover", dest="cross", help="Crossover method to use.", default="OnePoint")
-    parser.add_argument("-p", "--parentsel", dest="parentsel", help="Parent Selection method to use.", default="Best")
+    parser.add_argument("-p", "--parentsel", dest="parentsel", help="Parent Selection method to use.", default="Generational")
     parser.add_argument("-s", "--survsel", dest="survsel", help="Survivor Selection method to use.", default="Elitism")
-    parser.add_argument("-x", "--nexecs", dest="nexecs", help="Maximum number of evaluations of the fitness function.", default=int(3e5), type=int)
+    parser.add_argument("-g", "--ngen", dest="ngen", help="Maximum number of generations.", default=1000, type=int)
     parser.add_argument("-a", "--popsize", dest="popsize", help="Size of the population.", default=100, type=int)
     parser.add_argument("-n", "--nreps", dest="nreps", help="Number of times to run the algorithm.", default=1000, type=int)
-    parser.add_argument("-f", "--fileout", dest="fileout", help="Name of the output file.", default="result_GA-mhd-0.csv")
+    parser.add_argument("-f", "--fileout", dest="fileout", help="Name of the output file.", default=f"result_GA-pygad.csv")
     args = parser.parse_args()
 
-    run_ga(args.objective.upper(), args.dim, args.mut.upper(), args.cross.upper(), args.parentsel.upper(), args.survsel.upper(), args.nexecs, args.popsize, args.nreps, args.fileout)
+    run_ga(args.objective.lower(), args.dim, args.mut.lower(), args.cross.lower(), args.parentsel.lower(), args.survsel.lower(), args.ngen, args.popsize, args.nreps, args.fileout)
---
+++ verification/metaheuristic-designer/run_all.sh
@@ -0,0 +1,2 @@
+conda activate bench-mhd
+parallel python run_GA.py -o {1} -d {2} --ngen 300 -f result_GA-mhd-vs-pygad-{1}-d{2}.csv ::: 'Sphere' 'Rastrigin' 'Rosenbrock' 'Weierstrass' ::: 1 2 3 10 100
