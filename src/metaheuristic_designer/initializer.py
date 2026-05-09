5b83ea915111dc431306c24dfee148116718932f
Eugenio Lorente <46348947+eugenioLR@users.noreply.github.com>
2026-05-09 13:57:44 +0200

    docs: improve Initializer abstract class docstrings
    - Fix incorrect parameter documentation in generate_random() and generate_individual()
    - Add comprehensive parameter descriptions for __init__()
    - Add cross-references using See Also sections
    - Improve return value documentation
    - Add docstring for get_state() method

--- src/metaheuristic_designer/initializer.py
+++ src/metaheuristic_designer/initializer.py
@@ -20,10 +20,15 @@ class Initializer(ABC):
 
     Parameters
     ----------
-    pop_size: int, optional
-        Number of individuals to be generated.
-    encoding: Encoding, optional
-        Encoding that will be passed to each individual.
+    dimension : int
+        The dimensionality of the search space (length of individual vectors).
+    population_size : int, optional
+        Number of individuals to be generated in a population.
+    encoding : Encoding, optional
+        Encoding that transforms between phenotype and genotype representations.
+        If None, defaults to :class:`DefaultEncoding`.
+    random_state : RNGLike, optional
+        Random state for reproducible individual generation.
     """
 
     def __init__(self, dimension: int, population_size: int = 1, encoding: Optional[Encoding] = None, random_state: Optional[RNGLike] = None):
@@ -41,51 +46,64 @@ class Initializer(ABC):
     @abstractmethod
     def generate_random(self) -> Any:
         """
-        Generates a random individual.
-
-        Parameters
-        ----------
-        objfunc: ObjectiveFunc
-            Objective function that will be propagated to the individual.
+        Generates a random individual according to the initializer's distribution.
+        
+        This abstract method must be implemented by subclasses to define the
+        sampling strategy for individual generation. Implementations should use
+        :attr:`self.random_state` to ensure reproducibility.
 
         Returns
         -------
-        new_individual: Any
-            Newly generated individual.
+        new_individual : Any
+            Newly generated individual in genotype form. The dimensionality should
+            match :attr:`self.dimension`.
+        
+        See Also
+        --------
+        generate_individual : Wrapper method that typically delegates to this function.
+        generate_population : Generates an entire population using this method.
         """
 
     def generate_individual(self) -> Any:
         """
-        Define how an individual is initialized
-
-        Parameters
-        ----------
-        objfunc: ObjectiveFunc
-            Objective function that will be propagated to the individual.
+        Generates a single individual for the population.
+        
+        By default, this method delegates to :meth:`generate_random`. Subclasses
+        can override this to implement custom initialization logic while keeping
+        the random sampling strategy separated in :meth:`generate_random`.
 
         Returns
         -------
-        new_individual: Any
-            Newly generated individual.
+        new_individual : Any
+            Newly generated individual in genotype form.
+        
+        See Also
+        --------
+        generate_random : Abstract method defining the base sampling strategy.
+        generate_population : Generates multiple individuals using this method.
         """
 
         return self.generate_random()
 
     def generate_population(self, objfunc: ObjectiveFunc, n_individuals: Optional[int] = None) -> Population:
         """
-        Generate n_individual Individuals using the generate_individual method.
+        Generate n_individuals individuals using the generate_individual method.
 
         Parameters
         ----------
-        objfunc: ObjectiveFunc
-            Objective function that will be propagated to each individual.
-        n_individual: int, optional
-            Number of individuals to generate
+        objfunc : ObjectiveFunc
+            Objective function to be optimized by the population.
+        n_individuals : int, optional
+            Number of individuals to generate. If None, defaults to :attr:`self.pop_size`.
 
         Returns
         -------
-        generated_population: Population
-            Newly generated population.
+        generated_population : Population
+            Newly generated population with fitness calculated based on :obj:`objfunc`.
+        
+        See Also
+        --------
+        generate_individual : Generates individual solutions.
         """
 
         if n_individuals is None:
@@ -95,6 +113,14 @@ class Initializer(ABC):
         return Population(objfunc, genotype_matrix=population_matrix, encoding=self.encoding)
 
     def get_state(self):
+        """
+        Gets the current state of the initializer as a dictionary.
+        
+        Returns
+        -------
+        state : dict
+            Dictionary containing the class name and other relevant state information.
+        """
         data = {"class_name": self.__class__.__name__}
 
         return data
@@ -102,24 +128,40 @@ class Initializer(ABC):
 
 class InitializerFromLambda(Initializer):
     """
-    Initializer that generates individuals with vectors following an user-defined distribution.
+    Initializer that generates individuals using a user-defined function.
 
     Parameters
     ----------
-    generator: callable
+    generator : Callable
         Function that samples an user-defined probability distribution to generate individuals.
-    pop_size: int, optional
+        Should accept a random state as its first argument.
+    dimension : int
+        The dimensionality of the search space.
+    pop_size : int, optional
         Number of individuals to be generated.
-    encoding: Encoding, optional
+    encoding : Encoding, optional
         Encoding that will be passed to each individual.
+    random_state : RNGLike, optional
+        Random state for reproducible generation.
     """
 
     def __init__(
         self, generator: Callable, dimension: int, pop_size: int = 1, encoding: Optional[Encoding] = None, random_state: Optional[RNGLike] = None
     ):
+        """
+        Constructor for the InitializerFromLambda class.
+        """
         self.generator = generator
 
         super().__init__(dimension=dimension, population_size=pop_size, encoding=encoding, random_state=random_state)
 
     def generate_random(self) -> Any:
+        """
+        Generates a random individual by calling the user-defined generator function.
+        
+        Returns
+        -------
+        new_individual : Any
+            Individual generated by the user-provided generator function.
+        """
         return self.generator(self.random_state)
